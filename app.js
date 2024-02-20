e752d1a8d19f83c6b7f4ea0be19731b990335a57
Saad Ahmed <137560242+Saadgithubit@users.noreply.github.com>
2024-02-21 02:07:19 +0500

    Update

---
+++ app.js
@@ -0,0 +1,13 @@
+function goToOlx(){
+    window.location.href = 'https://olx-clone-ebon.vercel.app/'
+}
+function goToOlxGithub(){
+    window.location.href = 'https://github.com/Saadgithubit/Smit-react-Assignments/tree/main/Olx-Clone-Website'
+}
+
+function goToMeme(){
+    window.location.href = 'https://meme-generator-five-teal.vercel.app/'
+}
+function goToMemeGithub(){
+    window.location.href = 'https://github.com/Saadgithubit/Smit-Next-JS-Projects/tree/main/meme-generator'
+}
