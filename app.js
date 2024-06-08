dbf4723cc936b542fdad9e662e2f505e9b85e78d
Saadgithubit <saad45257@gmail.com>
2024-06-08 16:27:55 -0700

    add Themes

--- app.js
+++ app.js
@@ -1,43 +1,60 @@
-function goToOlx(){
+function goToOlx() {
     window.location.href = 'https://olx-clone-ebon.vercel.app/'
 }
-function goToOlxGithub(){
+function goToOlxGithub() {
     window.location.href = 'https://github.com/Saadgithubit/Smit-react-Assignments/tree/main/Olx-Clone-Website'
 }
 
-function goToMeme(){
+function goToMeme() {
     window.location.href = 'https://meme-generator-five-teal.vercel.app/'
 }
-function goToMemeGithub(){
+function goToMemeGithub() {
     window.location.href = 'https://github.com/Saadgithubit/Smit-Next-JS-Projects/tree/main/meme-generator'
 }
 
-function goToWeather(){
+function goToWeather() {
     window.location.href = 'https://currentweatherweb.netlify.app'
 }
-function goToWeatherGithub(){
+function goToWeatherGithub() {
     window.location.href = 'https://github.com/Saadgithubit/Smit-react-Assignments/tree/main/weather-app'
 }
-function goToSapphire(){
+function goToSapphire() {
     window.location.href = 'https://saphhire.vercel.app/'
 }
-function goToSapphireGithub(){
+function goToSapphireGithub() {
     window.location.href = 'https://github.com/Saadgithubit/Smit-react-Assignments/tree/main/Mernstack-Project'
 }
+
+var inp = document.getElementById('flexSwitchCheckChecked')
+var inpLabel = document.getElementById('label')
+inpLabel.innerHTML = 'Disable Dark Mode'
+var body = document.getElementById('body')
+body.className = 'darkTheme'
+inp.addEventListener('input', function (e) {
+    var isChecked = e.target.checked
+    if (isChecked === true) {
+        body.className = 'darkTheme'
+        inpLabel.innerHTML = 'Disable Dark Mode'
+    } else {
+        body.className = 'lightTheme'
+        inpLabel.innerHTML = 'Enable Dark Mode'
+    }
+    console.log(isChecked);
+})
 
 let xIcon = document.getElementById('x-icon')
 xIcon.className = 'hide'
 let navIcon = document.getElementById('nav-icon')
 let icons = false
 
-function showHide(){
+function showHide() {
     icons = !icons
     console.log(icons);
-    if(icons === true){
+    if (icons === true) {
         navIcon.className = 'hide'
         xIcon.className = ''
     }
-    else{
+    else {
         xIcon.className = 'hide'
         navIcon.className = ''
     }
